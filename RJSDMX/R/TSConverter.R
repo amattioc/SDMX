89b407d88efd7c8c556cd845dd8176d19dfecae2
bowerth <bo.werth@gmail.com>
2014-09-24 17:09:33 +0200

    Replace non UTF8 character

--- RJSDMX/R/TSConverter.R
+++ RJSDMX/R/TSConverter.R
@@ -1,6 +1,6 @@
 # Copyright 2010,2014 Bank Of Italy
 #
-# Licensed under the EUPL, Version 1.1 or – as soon they
+# Licensed under the EUPL, Version 1.1 or - as soon they
 # will be approved by the European Commission - subsequent
 # versions of the EUPL (the "Licence");
 # You may not use this work except in compliance with the
